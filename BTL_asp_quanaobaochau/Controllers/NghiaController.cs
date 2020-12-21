4899663b44d019210599fe77372492964fba2916
= <=>
2020-12-22 01:14:22 +0700

    cap nhat gio hang , nut quay lai home

--- BTL_asp_quanaobaochau/Controllers/NghiaController.cs
+++ BTL_asp_quanaobaochau/Controllers/NghiaController.cs
@@ -22,25 +22,9 @@ namespace BTL_asp_quanaobaochau.Controllers
         }
         public ActionResult muahang(int id)
         {
-            Console.WriteLine("mua hang");
             if (Session["giohang"] == null)
             {
-                GioHang a = new GioHang();
-                var sp = db.SanPhams.Where(s => s.MaSanPham == id).Single();
-                a.Id = sp.MaSanPham;
-                a.ten = sp.TenSanPham;
-                a.soluong = 1;
-                a.gia = Convert.ToInt32(sp.Gia);
-                List<GioHang> giohang = new List<GioHang>();
-                giohang.Add(a);
-                Session["giohang"] = giohang;
-                ViewBag.listsp = giohang;
-            }
-            else
-            {
-                List<GioHang> giohang = (List<GioHang>)Session["giohang"];
-                var test = giohang.Find(s => s.Id == id);
-                if (test == null)
+                try
                 {
                     GioHang a = new GioHang();
                     var sp = db.SanPhams.Where(s => s.MaSanPham == id).Single();
@@ -48,17 +32,48 @@ namespace BTL_asp_quanaobaochau.Controllers
                     a.ten = sp.TenSanPham;
                     a.soluong = 1;
                     a.gia = Convert.ToInt32(sp.Gia);
+                    List<GioHang> giohang = new List<GioHang>();
                     giohang.Add(a);
+                    Session["giohang"] = giohang;
+                    ViewBag.listsp = giohang;
+                }
+                catch (InvalidOperationException)
+                {
+                    ViewBag.listsp = null;
+                }
+                
+            }
+            else
+            {
+                List<GioHang> giohang = (List<GioHang>)Session["giohang"];
+                var index = giohang.FindIndex(sp => sp.Id == id);
+                if (index == -1)
+                {
+                    try
+                    {
+                        ViewBag.kiemtra = "null";
+                        GioHang a = new GioHang();
+                        var sp = db.SanPhams.Where(s => s.MaSanPham == id).Single();
+                        a.Id = sp.MaSanPham;
+                        a.ten = sp.TenSanPham;
+                        a.soluong = 1;
+                        a.gia = Convert.ToInt32(sp.Gia);
+                        giohang.Add(a);
+                    }catch(InvalidOperationException)
+                    {
+                        
+                    }
                 }
                 else
                 {
-                    test.soluong = test.soluong++;
+                    giohang[index].soluong++;
                 }
-
                 Session["giohang"] = giohang;
+                ViewBag.length = giohang.Count;
                 ViewBag.listsp = giohang;
             }
             return View();
         }
+        
     }
 }
